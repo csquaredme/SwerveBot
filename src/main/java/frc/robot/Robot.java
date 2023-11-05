1ac71ea5d4481ad26187e08cb8b0ba2b5e6e8640
csquaredme <csquaredme@users.noreply.github.com>
2023-11-05 15:49:09 -0800

    Add note to change Joystick from xbox sontroller

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 
 public class Robot extends TimedRobot {
-  private final XboxController m_controller = new XboxController(0);
+  private final XboxController m_controller = new XboxController(0); //Change to Joystick
   private final Drivetrain m_swerve = new Drivetrain();
 
   // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
